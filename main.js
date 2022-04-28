e157123c17eb4f21d0a052a82b410b39956570ef
Stanimir <stanimirkosev00@gmail.com>
2022-04-28 12:04:44 +0300

    page layout done

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("lqlq");
+(()=>{"use strict";!function(){const e=document.getElementById("parent"),t=document.createElement("div");t.textContent="To-do List",e.appendChild(t).className="header";const c=document.createElement("img");c.src="pictures/check.png",c.alt="check-pic",t.appendChild(c);const n=document.createElement("div");e.appendChild(n).className="sidebar";const d=document.createElement("div");e.appendChild(d).className="main";const a=document.createElement("div");e.appendChild(a).className="footer"}()})();
